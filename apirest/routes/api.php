eb365fc7981e133041d7dd240bac0d2671489f83
Gonzalo Lucio <gonzaloandreslucio@gmail.com>
2024-03-10 14:10:12 -0500

    Primer Commit

--- apirest/routes/api.php
+++ apirest/routes/api.php
@@ -23,6 +23,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 */
+Route::get('/test', function () {
+    return response()->json(['message' => 'Test route is working!'], 200);
+});
+Route::get('/test/ntest', function () {
+    return response()->json(['message' => 'Test2 route is working!'], 200);
+});
 //Rutas protegidas
     Route::post('/assign-role', [RoleAssignmentController::class, 'assignRole']);
 
@@ -53,9 +59,14 @@ Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logo
 
 Route::get('/root/backup/download/{file}', [App\Http\Controllers\Root\BackupController::class, 'download'])->name('backup.download');
 // Agrupar todas las rutas que estarán bajo el prefijo 'root'
-Route::prefix('secretaria')->middleware(['auth:sanctum', 'secretaria'])->group(function () {
+Route::prefix('secretaria')->name('secretaria.')->middleware(['auth:sanctum', 'secretaria'])->group(function () {
     Route::get('/tipo_documentos', [App\Http\Controllers\Secretaria\TipoDocumentoController::class, 'index']);
-    Route::apiResource('cursos', App\Http\Controllers\Secretaria\CursoController::class);
+    Route::apiResource('cursos', App\Http\Controllers\Secretaria\CursoController::class)
+        ->names([
+            'index' => 'secretaria.cursos.index',
+            'show' => 'secretaria.cursos.show',
+            // Agrega los demás métodos si es necesario
+        ]);
     Route::get('cursos/pa-matricular/{id}', [App\Http\Controllers\Secretaria\CursoController::class, 'paMatricular']);
     Route::post('alumnos/add', [App\Http\Controllers\Secretaria\AlumnoController::class, 'add']);
     Route::get('alumnos/nuevos', [App\Http\Controllers\Secretaria\AlumnoController::class, 'nuevos']);
@@ -71,7 +82,7 @@ Route::prefix('secretaria')->middleware(['auth:sanctum', 'secretaria'])->group(f
     Route::get('/auth', [App\Http\Controllers\AuthController::class, 'auth']);
     Route::put('/auth/update', [App\Http\Controllers\AuthController::class, 'update']);
 });
-Route::prefix('coordinacion')->middleware(['auth:sanctum', 'coordinacion'])->group(function () {
+Route::prefix('coordinacion')->name('coordinacion.')->middleware(['auth:sanctum', 'coordinacion'])->group(function () {
     Route::apiResource('lectivos', App\Http\Controllers\Coordinacion\LectivoController::class);
     Route::apiResource('sedes', App\Http\Controllers\Coordinacion\SedeController::class);
     Route::apiResource('grados', App\Http\Controllers\Coordinacion\GradoController::class);
@@ -90,7 +101,7 @@ Route::prefix('coordinacion')->middleware(['auth:sanctum', 'coordinacion'])->gro
     Route::put('/auth/update', [App\Http\Controllers\AuthController::class, 'update']);
 });
 
-Route::prefix('root')->middleware(['auth:sanctum', 'root'])->group(function () {
+Route::prefix('root')->name('root.')->middleware(['auth:sanctum', 'root'])->group(function () {
     // Ruta ejemplo para la gestión de usuarios dentro del directorio 'Root'
     Route::get('/backup', [App\Http\Controllers\Root\BackupController::class, 'index']);
 
